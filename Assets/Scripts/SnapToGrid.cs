16c2c1ee708db3a6204803993d90cfc7e51bb22a
el3031 <67562724+el3031@users.noreply.github.com>
2022-05-06 15:00:09 -0400

    discretized position mmade. now just need to try this script out on an image target

---
+++ Assets/Scripts/SnapToGrid.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour
+{
+    [SerializeField] private GameObject grid;
+    private bool snap = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (snap)
+        {
+            Vector3 rawPos = transform.localPosition;
+            Vector3 snappedPos = new Vector3(Mathf.Round(rawPos.x), Mathf.Round(rawPos.y), Mathf.Round(rawPos.z));
+            Debug.Log(snappedPos);
+            transform.localPosition = snappedPos;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Grid"))
+        {
+            Debug.Log("snapping");
+            snap = true;
+            this.transform.parent = grid.transform;
+        }
+        else
+        {
+            Debug.Log("poop");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Grid"))
+        {
+            snap = false;
+            this.transform.parent = null;
+        }
+    }
+}
