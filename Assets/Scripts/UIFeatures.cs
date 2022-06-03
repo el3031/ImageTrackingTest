535792c5f29d376ef199752196a03b8465bac22c
el3031 <67562724+el3031@users.noreply.github.com>
2022-06-02 19:32:42 -0700

    UI features and particle system

---
+++ Assets/Scripts/UIFeatures.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFeatures : MonoBehaviour
+{
+    [SerializeField] private Text year;
+    [SerializeField] private int currentYear;
+    
+    [SerializeField] private float co2MaxDelta;
+    [SerializeField] private float co2MinDelta;
+    [SerializeField] private Slider co2Meter;
+
+    public void OnEndTurn()
+    {
+        currentYear += 10;
+        year.text = (currentYear).ToString();
+        
+        float co2Delta = Random.Range(co2MinDelta, co2MaxDelta);
+        co2Meter.value += co2Delta;
+    }
+}
